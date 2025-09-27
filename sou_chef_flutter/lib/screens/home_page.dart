f7acd28d4bbf4795c3d3ca5c94b901e2912d90d9
Rishit <rishit.gautam2005@gmail.com>
2025-09-27 15:50:18 +0530

    feat: Added a drawer and sign out.

--- sou_chef_flutter/lib/screens/home_page.dart
+++ sou_chef_flutter/lib/screens/home_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sou_chef_flutter/widgets/my_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sou-Chef'),
+      ),
+      drawer: MyDrawer(),
+    );
   }
 }
---
+++ sou_chef_flutter/lib/widgets/my_drawer.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:sou_chef_flutter/screens/intro_screen.dart';
+
+class MyDrawer extends StatelessWidget {
+  MyDrawer({super.key});
+
+  void signUserOut(BuildContext context) {
+    FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context, 
+      MaterialPageRoute(builder: (context) => const IntroScreen()),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: const Text("Active Account."), 
+            accountEmail: Text(user?.email ?? "No email"),
+            currentAccountPicture: const CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(Icons.person, size: 40),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.grey[900],
+            ),
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("Home"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.support_agent_sharp),
+            title: const Text("Help"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.feedback),
+            title: const Text("Feedback"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.person_add),
+            title: const Text("Invite Friend"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.share),
+            title: const Text("Rate our app"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text("About us"),
+            onTap: () => {
+              Navigator.of(context).pop(),
+            },
+          ),
+
+          const Divider(),
+
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: const Text("Sign Out"),
+            onTap: () => signUserOut(context),
+          )
+        ],
+      )
+    );
+  }
+}
